b4e5f9186afff917bf53ddc1fe6d0cc0a165e4a4
Dr Bob <Dr Bob@DrBob-PC>
2017-08-04 03:29:22 -0700

    Tester added
    Algorithm partially working, but not working properly

--- GaussJordan/src/gaussJordan/GaussJordanTest.java
+++ GaussJordan/src/gaussJordan/GaussJordanTest.java
@@ -3,24 +3,25 @@ package gaussJordan;
 import java.util.ArrayList;
 
 public class GaussJordanTest {
-	public static int main(String[] args){
+	public static void main(String[] args){
 		System.out.println("Bob's rref using the Gauss-Jordan method, following matlab formatting, except you must put a space between each element, including: ';' '[', ']'");
 		System.out.println("Ex: [ 1 2 3 ; 14 15 16 ; 67 258 59 ]");
 		System.out.println("Also, tester only works with argument input.");
 		
 		//double[][] input = getDouble2D(args);
 		double[][] input = new double[3][];
-		input[0] = new double[]{1d, 2d, 3d, 4d};
-		input[1] = new double[]{2d, 4d, 6d, 8d};
+		input[0] = new double[]{0d, 2d, 3d, 4d};
+		input[1] = new double[]{0d, 4d, 6d, 8d};
 		input[2] = new double[]{3d, 3d, 3d, 3d};
 		
 		print(input);
 		
+
+		System.out.println("");
+		
 		double[][] rref = GaussJordan.rref(input);
 		
 		print(rref);
-		
-		return 0;
 	}
 	
 	private static void print(double[][] matrix){
